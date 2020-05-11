c4a9759d45e15fb2c52fed7fc202d2271e5b3c0a
ajith <ajith.git@gmail.com>
2020-05-11 10:32:26 +0530

    edited

--- physics/potential.py
+++ physics/potential.py
@@ -2,14 +2,16 @@ from pylab import *          # to install, sudo apt install python3-matplotlib
 
 q1 = 1.60217662e-19
 q2 = q1
-e0 = 8.85418782e-12
-hcross = 6.62607004e-34/(2*pi)
-m1 = 1.6726219e-27      # proton
-m2 = 9.10938356e-31     # electron
-mu = m1*m2/(m1+m2)
+e0 = 8.85418782e-12         	# permittivity of free space
+hcross = 6.62607004e-34 / (2*pi)
+m1 = 1.6726219e-27      		# proton mass
+m2 = 9.10938356e-31     		# electron mass
+mu = m1*m2/(m1+m2)				# reduced mass
 
 
-ra = linspace(0.2, 5, 200) * 1e-10   # r from 10 to 100 Fermi
+rmin = 0.2e-10
+rmax = 5.0e-10
+ra = linspace(rmin, rmax, 200)  # range of radius for calculation
 
 l = 1
 
@@ -18,12 +20,13 @@ Vl = l * (l + 1) * hcross ** 2 / (2 * mu * ra **2)
 Vr = -q1 * q2 / (4*pi*e0*ra)
 
 
-xlim(0, 5e-10)
-ylim(-2e-18,2e-18)
+xlim(0, rmax)
+ylim(-2e-18,2e-18)      # adjusted to view the most relevant part of the graph
+
 xlabel('radius')
 ylabel('potential')
 
-plot([0,5e-10], [0,0])    # x-axis
+plot([0, rmax], [0,0])    # x-axis
 
 plot(ra, Vr)
 plot(ra, Vl)
